afbb7ef45ef937f965ee68fe774c0d34df2798e7
hastyjaybird <gaia.revolts@gmail.com>
2016-08-23 23:02:13 -0700

    fixed post, got flask working to print values to file

--- ESP8366/ESP8366.ino
+++ ESP8366/ESP8366.ino
@@ -4,6 +4,12 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include <ESP8266WiFi.h>
+
+
+//ID of module
+String id = "01";
+
+
 //
 //const char* ssid     = "Jodog's Network";
 //const char* password = "R1234567";
@@ -11,11 +17,15 @@
 //const char* ssid     = "hearth-new";
 //const char* password = "education";
 //
-const char* ssid = "ffflava";
-const char* password = "packettt";
-const char* host = "192.168.1.137";
+//const char* ssid = "ffflava";
+//const char* password = "packettt";
 
-char outBuf[128];
+const char* ssid = "sudomesh";
+const char* password = "";
+
+const char* host = "192.168.196.128";
+
+//char outBuf[128];
  
 //String current = "666";
 String current() {
@@ -36,10 +46,11 @@ void setup() {
   Serial.print("Connecting to ");
   Serial.println(ssid);
   
-  WiFi.begin(ssid, password);
+//  WiFi.begin(ssid, password);
+  WiFi.begin(ssid);
   
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    delay(1000);
     Serial.println("searching for long range comms...");
   }
 
@@ -54,7 +65,7 @@ void setup() {
 int value = 0;
 
 void loop() {
-  delay(500);
+  delay(1000);
   ++value;
   
   Serial.print("connecting to ");
@@ -81,16 +92,48 @@ void loop() {
 // Content-Length: 10
 // Content-Type: application/x-www-form-urlencoded
 
+//
+
   client.print("POST /data HTTP/1.1\r\n"  
-               "User-Agent: curl/7.35.0\r\n"
+               "User-Agent: curl/7.38.0\r\n"
                "Host: 192.168.1.137:5000\r\n"
                "Accept: */*\r\n"
-               "Content-Length: 10\r\n"
-               "Content-Type: application/x-www-form-urlencoded\r\n");
-  client.print("pin1=54321");
-  //client.print("pin1="+current()+"\r\n");
-  client.print("Connection: close\r\n");            
-  delay(500);
+               "Content-Length: 19\r\n"
+               "Content-Type: application/x-www-form-urlencoded\r\n"
+               "\r\n"
+               "pin1="+current()+"&"
+               "name="+id+"\r\n");
+               
+             
+               
+    
+
+//
+//  client.println("POST /data HTTP/1.1");
+//  client.println("Host: 192.168.1.137");
+//  client.println("Content-Type: text/html");
+//  client.println("Content-Length: 3");
+//  client.println(thisdata);
+//  client.println("Connection: close\r\n");
+  
+
+//
+//  
+//  client.println(F("Connection: close\r\n application/x-www-form-urlencoded"));
+//  sprintf(outBuf,"Content-Length: 3");
+//  client.println(outBuf);
+//
+//   // send the body (variables)
+//  client.print(thisdata);
+
+
+
+
+
+
+            
+  delay(1000);
+
   
   // Read all the lines of the reply from server and print them to Serial
   while(client.available()){
--- flasktest.py
+++ flasktest.py
@@ -1,19 +1,27 @@
 from flask import Flask
 from flask import request
+import time
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!\r\n'
+# create file (csv+ datestamp. )
+
 
 @app.route('/data', methods=['POST'])
 def current():
-	current = request.form['pin1']
-	# assert request.method == 'POST'
-	# assert request.path == '/data'
+	current = request.form['pin1'].strip()
+	name = request.form['name'].strip()
+
+	fp = open("somefile", "a")
+	fp.write(str(time.time()) + "," + name + "," + current + "\r\n")
+	fp.close()
 	print current
 	return "done"
-	
+
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
